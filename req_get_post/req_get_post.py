38196d2286dd8d6218f39e56713a39cb40c1337c
VladimirL86 <48181020+VladimirL86@users.noreply.github.com>
2019-03-14 02:30:54 +0300

    Update req_get_post.py with exception processing

--- req_get_post/req_get_post.py
+++ req_get_post/req_get_post.py
@@ -6,32 +6,46 @@ import time
 
 
 #GET request
-URL = sys.argv[1] + "/commands"
-PARAMS = {'aim': 'python_developer_job'}
-r = requests.get(url = URL, params = PARAMS)
-info = r.json()
-command = info['data']
-
-
-#processing
-if command == 'wrong_command':
-    result = 'error'
+if len(sys.argv) != 2:
+  print('Error: Wrong number of arguments!')
 else:
-    result = subprocess.check_output(command.split())
+  URL = sys.argv[1] + "/commands"
+  PARAMS = {'aim': 'python_developer_job'}
+  try:
+    r = requests.get(url = URL, params = PARAMS)
+  except Exception:
+    print('Error: Wrong URL!')
+  else:
+    try:
+      info = r.json()
+    except Exception:
+      print('Error: Wrong URL!')
+    else:
+      if not URL.count('https'):
+        print('Error: Wrong URL! Replace "http" with "https".')
+      else:
+        command = info['data']
+
+    
+#processing
+        if command == 'wrong_command':
+          result = 'error'
+        else:
+          result = subprocess.check_output(command.split())
 
 
 #logging
-path_to_logs = '/usr/local/bin/requests_log.txt'
-if not os.path.isfile(path_to_logs):
-    filehandle = open(path_to_logs, 'w')
-else:
-    filehandle = open(path_to_logs, 'a')
-filehandle.write(time.asctime() + ' : ')
-filehandle.write(command + ' : ' + str(result) + '\n')
-filehandle.close()
+        path_to_logs = '/usr/local/bin/requests_log.txt'
+        if not os.path.isfile(path_to_logs):
+          filehandle = open(path_to_logs, 'w')
+        else:
+          filehandle = open(path_to_logs, 'a')
+        filehandle.write(time.asctime() + ' : ')
+        filehandle.write(command + ' : ' + str(result) + '\n')
+        filehandle.close()
 
 
 #POST request
-API_ENDPOINT = sys.argv[1] + "/commands"
-DATA = {'command': command, 'result': str(result)}
-requests.post(url = API_ENDPOINT, data = DATA)
+        API_ENDPOINT = URL
+        DATA = {'command': command, 'result': str(result)}
+        requests.post(url = API_ENDPOINT, data = DATA)
